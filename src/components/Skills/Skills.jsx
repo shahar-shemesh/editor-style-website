50b38253b919514b374e6a2c32e4363d06fb1300
shahar-shemesh <shahar@usa.com>
2024-05-21 22:32:34 +0300

    skills added

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,16 +1,38 @@
 
 import classes from './Skills.module.css';
+import DATA from '../../data/index.json';
 
 
-export default function Skills(){
+export default function Skills() {
 
 
   return (
     <section tabIndex="0" className={classes.skills}>
 
-      SKILLS
+      <ul className={classes.skillList}>
 
-    </section>  
+        {DATA?.skills?.map((item, index) => (
+
+          <div key={index} className={classes.skillCard}>
+
+            <p className={classes.subject}>.{item.title}</p>
+
+            <ul className={classes.stack}>
+              {item?.stack?.map((skill) => (
+                <li key={skill} className={classes.skillItem}>
+                  <i className={`${classes.icon} bx bxl-${skill.toLowerCase()} bx-sm`}></i>
+                  <p className={classes.skillName}>{skill}</p>
+                </li>
+              ))}
+            </ul>
+
+          </div>
+
+        ))}
+      
+      </ul>
+
+    </section>
   );
 };
 
